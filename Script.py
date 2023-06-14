b2acaf144217c6705410366320887b4b04d63fd1
Wolf404 <96954406+WBs234@users.noreply.github.com>
2023-06-14 01:08:13 -0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -74,17 +74,17 @@ while True:
                pin2 = file.readlines()
                vx=str(pin1)+str(pin2)
                vy=int(vx)
-                def calculate_wps_pin(last_digit):
-                    possible_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-                    combinations = itertools.product(range(10), repeat=7)
-                    for combination in combinations:
-                        if (sum(combination) * 3 + last_digit) % 10 == 0:
-                            pin = ''.join(str(digit) for digit in combination) + str(last_digit)
-                            return pin
-                        return None
-                seven_digits = vy
-                    last_digit = calculate_wps_pin(int(seven_digits))
-                    lu=last_digit
+                   def calculate_wps_pin(last_digit):
+                       possible_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+                       combinations = itertools.product(range(10), repeat=7)
+                       for combination in combinations:
+                           if (sum(combination) * 3 + last_digit) % 10 == 0:
+                               pin = ''.join(str(digit) for digit in combination) + str(last_digit)
+                               return pin
+                           return None
+                   seven_digits = vy
+                       last_digit = calculate_wps_pin(int(seven_digits))
+                       lu=last_digit
                 px=vx+str(lu)
                 pin = int(px)
                 os.system("clear")
